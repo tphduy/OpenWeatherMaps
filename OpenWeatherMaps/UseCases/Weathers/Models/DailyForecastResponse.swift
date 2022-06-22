5f5dc3fd48e420f5f75d313a89feeb6d7b3e0e0c
Duy Tran <tphduy@gmail.com>
2022-06-22 11:53:11 +0700

    Implement `DailyForecastResponse`

---
+++ OpenWeatherMaps/UseCases/Weathers/Models/DailyForecastResponse.swift
@@ -0,0 +1,24 @@
+//
+//  DailyForecastResponse.swift
+//  OpenWeatherMaps
+//
+//  Created by Duy Tran on 22/06/2022.
+//
+
+import Foundation
+
+/// An object abstracts the daily forecasts of a city that will be returned from a remote database.
+///
+/// References: https://openweathermap.org/forecast16
+struct DailyForecastResponse: Codable {
+    /// A place where the forecast data was returned from.
+    let city: City
+    /// A list of daily forecasts.
+    let forecasts: [Forecast]
+    
+    /// A type that can be used as a key for encoding and decoding.
+    enum CodingKeys: String, CodingKey {
+        case city
+        case forecasts = "list"
+    }
+}
