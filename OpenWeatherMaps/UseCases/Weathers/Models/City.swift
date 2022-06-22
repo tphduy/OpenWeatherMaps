e455d94fb7d89f08330078ef7bca5ebcd45d2c27
Duy Tran <tphduy@gmail.com>
2022-06-22 11:52:41 +0700

    Implement `City` model

---
+++ OpenWeatherMaps/UseCases/Weathers/Models/City.swift
@@ -0,0 +1,20 @@
+//
+//  City.swift
+//  OpenWeatherMaps
+//
+//  Created by Duy Tran on 22/06/2022.
+//
+
+import Foundation
+
+/// A large human settlement.
+struct City: Codable {
+    /// A unique identifier.
+    let id: Int
+    /// The city name.
+    let name: String?
+    /// The country that this city belongs to.
+    let country: String?
+    /// The time zone where this city located
+    let timezone: Int?
+}
