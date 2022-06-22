d42e976be1a043b70e7824967314b200f1702fef
Duy Tran <tphduy@gmail.com>
2022-06-22 11:52:25 +0700

    Implement `Temperature` model

---
+++ OpenWeatherMaps/UseCases/Weathers/Models/Temperature.swift
@@ -0,0 +1,34 @@
+//
+//  Temperature.swift
+//  OpenWeatherMaps
+//
+//  Created by Duy Tran on 22/06/2022.
+//
+
+import Foundation
+
+/// An object abstracts the measured amount of heat in a place.
+struct Temperature: Codable {
+    /// Day temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    let day: Double?
+    /// Night temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    let night: Double?
+    /// Min daily temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    let min: Double?
+    /// Max daily temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    let max: Double?
+    /// Evening temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    let evening: Double?
+    /// Morning temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    let morning: Double?
+    
+    /// A type that can be used as a key for encoding and decoding.
+    enum CodingKeys: String, CodingKey {
+        case day
+        case night
+        case min
+        case max
+        case evening = "eve"
+        case morning = "morn"
+    }
+}
