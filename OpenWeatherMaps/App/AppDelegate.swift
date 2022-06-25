c06f9d2342e3bbc13364fe4416a26e214416d323
Duy Tran <tphduy@gmail.com>
2022-06-25 12:29:34 +0700

    Install Reachability library

--- OpenWeatherMaps/App/AppDelegate.swift
+++ OpenWeatherMaps/App/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Reachability
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        Reachability.shared.bootstrap()
         return true
     }
 
