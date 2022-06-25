fc27cf4991eee49c7b60994c57617883052517d1
Duy Tran <tphduy@gmail.com>
2022-06-25 21:28:24 +0700

    Display more information in `ForecastCollectionViewCell`

--- OpenWeatherMaps/Scenes/DailyForecasts/ForecastCollectionViewCell.swift
+++ OpenWeatherMaps/Scenes/DailyForecasts/ForecastCollectionViewCell.swift
@@ -18,9 +18,43 @@ final class ForecastCollectionViewCell: UICollectionViewCell {
         return view
     }()
     
+    /// A label that displays the averate temperature of a day.
+    private(set) lazy var averageTemperatureLabel: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    /// A label that displays the pressure.
+    private(set) lazy var pressureLabel: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    /// A label that displays the humidity.
+    private(set) lazy var humidityLabel: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    /// A label that displays the description.
+    private(set) lazy var descriptionLabel: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     /// A vertical stack view at the leading that contains all informative labels.
     private(set) lazy var leadingStackView: UIStackView = {
-        let view = UIStackView(arrangedSubviews: [dateLabel])
+        let view = UIStackView(arrangedSubviews: [
+            dateLabel,
+            averageTemperatureLabel,
+            pressureLabel,
+            humidityLabel,
+            descriptionLabel,
+        ])
         view.axis = .vertical
         view.alignment = .leading
         view.spacing = 8
@@ -63,8 +97,73 @@ final class ForecastCollectionViewCell: UICollectionViewCell {
     }
     
     /// Configure the appearance to displays a forecast.
-    /// - Parameter forecast: The expected weather conditions what is judged likely to happen in the future.
+    /// - Parameters:
+    ///   - forecast: The expected weather conditions what is judged likely to happen in the future.
+    ///   - dateFormatter: A formatter that converts between dates and their textual representations.
     func configure(withForecast forecast: Forecast, dateFormatter: DateFormatter) {
-        dateLabel.text = dateFormatter.string(from: forecast.date)
+        dateLabel.text = makeDate(forecast: forecast, dateFormatter: dateFormatter)
+        averageTemperatureLabel.text = makeAverageTemperature(forecast: forecast)
+        pressureLabel.text = makePressure(forecast: forecast)
+        humidityLabel.text = makeHumidity(forecast: forecast)
+        descriptionLabel.text = makeDescription(forecast: forecast)
+        // Hide all empty labels.
+        [dateLabel, averageTemperatureLabel, pressureLabel, humidityLabel, descriptionLabel]
+            .forEach { $0.isHidden = $0.text?.isEmpty ?? true }
+    }
+    
+    // MARK: Utilities
+    
+    /// Make a text that describes the average temperature of forecast.
+    /// - Parameters:
+    ///   - forecast: The expected weather conditions what is judged likely to happen in the future.
+    ///   - dateFormatter: A formatter that converts between dates and their textual representations.
+    /// - Returns: A localized text.
+    private func makeDate(forecast: Forecast, dateFormatter: DateFormatter) -> String {
+        let date = dateFormatter.string(from: forecast.date)
+        let format = NSLocalizedString("Date: %@", comment: "Date: %@")
+        let result = String(format: format, date)
+        return result
+    }
+    
+    /// Make a text that describes the average temperature of forecast.
+    /// - Parameter forecast: The expected weather conditions what is judged likely to happen in the future.
+    /// - Returns: An empty text if the temperature data is none, otherwise, a localized text.
+    private func makeAverageTemperature(forecast: Forecast) -> String {
+        let temperatures = [forecast.temperature?.min, forecast.temperature?.max].compactMap { $0 }
+        guard !temperatures.isEmpty else { return "" }
+        let averageTemperature = Int(temperatures.reduce(0, +) / Double(temperatures.count))
+        let format = NSLocalizedString("Average Temperature: %d", comment: "Average Temperature: %d")
+        let result = String(format: format, averageTemperature)
+        return result
+    }
+    
+    /// Make a text that describes the pressure of forecast.
+    /// - Parameter forecast: The expected weather conditions what is judged likely to happen in the future.
+    /// - Returns: An empty text if the pressure data is none, otherwise, a localized text.
+    private func makePressure(forecast: Forecast) -> String {
+        guard let pressure = forecast.pressure else { return "" }
+        let format = NSLocalizedString("Pressure: %d", comment: "Pressure: %d")
+        let result = String(format: format, Int(pressure))
+        return result
+    }
+    
+    /// Make a text that describes the humidity of forecast.
+    /// - Parameter forecast: The expected weather conditions what is judged likely to happen in the future.
+    /// - Returns: An empty text if the humidity data is none, otherwise, a localized text.
+    private func makeHumidity(forecast: Forecast) -> String {
+        guard let humidity = forecast.humidity else { return "" }
+        let format = NSLocalizedString("Humidity: %d", comment: "Humidity: %d")
+        let result = String(format: format, Int(humidity))
+        return result
+    }
+    
+    /// Make a text that describes the weather of forecast.
+    /// - Parameter forecast: The expected weather conditions what is judged likely to happen in the future.
+    /// - Returns: An empty text if the weather data is none, otherwise, a localized text.
+    private func makeDescription(forecast: Forecast) -> String {
+        guard let description = forecast.weather?.first?.desc else { return "" }
+        let format = NSLocalizedString("Description: %@", comment: "Description: %@")
+        let result = String(format: format, description)
+        return result
     }
 }
--- OpenWeatherMapsTests/Scenes/DailyForcasts/ForecastCollectionViewCellTests.swift
+++ OpenWeatherMapsTests/Scenes/DailyForcasts/ForecastCollectionViewCellTests.swift
@@ -11,17 +11,25 @@ import XCTest
 final class ForecastCollectionViewCellTests: XCTestCase {
     // MARK: Misc
     
+    private var forecast: Forecast!
     private var dateFormatter: DateFormatter!
     private var sut: ForecastCollectionViewCell!
     
     // MARK: Life Cycle
 
     override func setUpWithError() throws {
+        forecast = Forecast(
+            date: Date(),
+            temperature: Temperature(min: 0, max: 10),
+            pressure: 1,
+            humidity: 1,
+            weather: [Forecast.Weather(main: "Clear", desc: "sky is clear")])
         dateFormatter = makeDateFormatter()
         sut = ForecastCollectionViewCell()
     }
 
     override func tearDownWithError() throws {
+        forecast = nil
         dateFormatter = nil
         sut = nil
     }
@@ -35,11 +43,65 @@ final class ForecastCollectionViewCellTests: XCTestCase {
     // MARK: Test Case - configure(withForecast:)
     
     func test_configureWithForecast() throws {
-        let forecast = Forecast.dummy
+        let dateFormat = NSLocalizedString("Date: %@", comment: "Date: %@")
+        let date = String(format: dateFormat, dateFormatter.string(from: forecast.date))
+        let averageTemperatureFormat = NSLocalizedString("Average Temperature: %d", comment: "Average Temperature: %d")
+        let averageTemperature = String(format: averageTemperatureFormat, 5)
+        let pressureFormat = NSLocalizedString("Pressure: %d", comment: "Pressure: %d")
+        let pressure = String(format: pressureFormat, 1)
+        let humidityFormat = NSLocalizedString("Humidity: %d", comment: "Humidity: %d")
+        let humidity = String(format: humidityFormat, 1)
+        let descriptionFormat = NSLocalizedString("Description: %@", comment: "Description: %@")
+        let description = String(format: descriptionFormat, "sky is clear")
         
         sut.configure(withForecast: forecast, dateFormatter: dateFormatter)
         
-        XCTAssertEqual(sut.dateLabel.text, dateFormatter.string(from: forecast.date))
+        XCTAssertEqual(sut.dateLabel.text, date)
+        XCTAssertFalse(sut.dateLabel.isHidden)
+        XCTAssertEqual(sut.averageTemperatureLabel.text, averageTemperature)
+        XCTAssertFalse(sut.averageTemperatureLabel.isHidden)
+        XCTAssertEqual(sut.pressureLabel.text, pressure)
+        XCTAssertFalse(sut.pressureLabel.isHidden)
+        XCTAssertEqual(sut.humidityLabel.text, humidity)
+        XCTAssertFalse(sut.humidityLabel.isHidden)
+        XCTAssertEqual(sut.descriptionLabel.text, description)
+        XCTAssertFalse(sut.descriptionLabel.isHidden)
+    }
+    
+    func test_configureWithForecast_whenDateIsNone() throws {
+        let forecast = Forecast(
+            date: Date(),
+            temperature: nil,
+            pressure: nil,
+            humidity: nil,
+            weather: nil)
+        
+        sut.configure(withForecast: forecast, dateFormatter: dateFormatter)
+        
+        XCTAssertEqual(sut.averageTemperatureLabel.text, "")
+        XCTAssertTrue(sut.averageTemperatureLabel.isHidden)
+        XCTAssertEqual(sut.pressureLabel.text, "")
+        XCTAssertTrue(sut.pressureLabel.isHidden)
+        XCTAssertEqual(sut.humidityLabel.text, "")
+        XCTAssertTrue(sut.humidityLabel.isHidden)
+        XCTAssertEqual(sut.descriptionLabel.text, "")
+        XCTAssertTrue(sut.descriptionLabel.isHidden)
+    }
+    
+    func test_configureWithForecast_whenMinimumTemperatureIsNone() throws {
+        let format = NSLocalizedString("Average Temperature: %d", comment: "Average Temperature: %d")
+        let averageTemperature = String(format: format, 10)
+        let forecast = Forecast(
+            date: Date(),
+            temperature: Temperature(min: nil, max: 10),
+            pressure: nil,
+            humidity: nil,
+            weather: nil)
+        
+        sut.configure(withForecast: forecast, dateFormatter: dateFormatter)
+        
+        XCTAssertEqual(sut.averageTemperatureLabel.text, averageTemperature)
+        XCTAssertFalse(sut.averageTemperatureLabel.isHidden)
     }
 }
 
