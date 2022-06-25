1b6940dce8694dd3d98cb73b28904a8ab2bf6ef5
Duy Tran <tphduy@gmail.com>
2022-06-25 16:35:16 +0700

    Implement a date label for the `ForecastCollectionViewCell`

--- OpenWeatherMaps/Scenes/DailyForecasts/ForecastCollectionViewCell.swift
+++ OpenWeatherMaps/Scenes/DailyForecasts/ForecastCollectionViewCell.swift
@@ -9,9 +9,62 @@ import UIKit
 
 /// A collection view cell that displays a forecast.
 final class ForecastCollectionViewCell: UICollectionViewCell {
+    // MARK: Uis
+    
+    /// A label that displays the date a forecase was publised.
+    private(set) lazy var dateLabel: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    /// A vertical stack view at the leading that contains all informative labels.
+    private(set) lazy var leadingStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [dateLabel])
+        view.axis = .vertical
+        view.alignment = .leading
+        view.spacing = 8
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    /// A horizontal stack view that contains all subviews.
+    private(set) lazy var masterStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [leadingStackView])
+        view.axis = .horizontal
+        view.alignment = .center
+        view.spacing = 8
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    // MARK: Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
     // MARK: Side Effects
+    
+    /// Configure the appearance and the view hierarchy.
+    private func setupLayout() {
+        contentView.addSubview(masterStackView)
+        NSLayoutConstraint.activate([
+            masterStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            masterStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            masterStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
+            masterStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+        ])
+    }
     
     /// Configure the appearance to displays a forecast.
     /// - Parameter forecast: The expected weather conditions what is judged likely to happen in the future.
-    func configure(withForecast forecast: Forecast) {}
+    func configure(withForecast forecast: Forecast, dateFormatter: DateFormatter) {
+        dateLabel.text = dateFormatter.string(from: forecast.date)
+    }
 }
--- OpenWeatherMapsTests/Scenes/DailyForcasts/ForecastCollectionViewCellTests.swift
+++ OpenWeatherMapsTests/Scenes/DailyForcasts/ForecastCollectionViewCellTests.swift
@@ -11,21 +11,45 @@ import XCTest
 final class ForecastCollectionViewCellTests: XCTestCase {
     // MARK: Misc
     
+    private var dateFormatter: DateFormatter!
     private var sut: ForecastCollectionViewCell!
     
     // MARK: Life Cycle
 
     override func setUpWithError() throws {
+        dateFormatter = makeDateFormatter()
         sut = ForecastCollectionViewCell()
     }
 
     override func tearDownWithError() throws {
+        dateFormatter = nil
         sut = nil
+    }
+    
+    // MARK: Test Case - setupLayout()
+    
+    func test_setupLayout() throws {
+        XCTAssertTrue(sut.masterStackView.isDescendant(of: sut.contentView))
     }
     
     // MARK: Test Case - configure(withForecast:)
     
     func test_configureWithForecast() throws {
-        sut.configure(withForecast: .dummy)
+        let forecast = Forecast.dummy
+        
+        sut.configure(withForecast: forecast, dateFormatter: dateFormatter)
+        
+        XCTAssertEqual(sut.dateLabel.text, dateFormatter.string(from: forecast.date))
+    }
+}
+
+extension ForecastCollectionViewCellTests {
+    // MARK: Utilities
+    
+    private func makeDateFormatter() -> DateFormatter {
+        let result = DateFormatter()
+        result.dateStyle = .short
+        result.timeStyle = .none
+        return result
     }
 }
