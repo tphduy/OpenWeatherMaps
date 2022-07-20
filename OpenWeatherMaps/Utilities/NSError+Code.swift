d2f78d3f703e2d01bd48769f47a23fba2cf2764d
Duy Tran <tphduy@gmail.com>
2022-07-20 23:23:24 +0700

    Add more descriptions

--- OpenWeatherMaps/Utilities/NSError+Code.swift
+++ OpenWeatherMaps/Utilities/NSError+Code.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 extension NSError {
+    /// An enum declare the well-known error codes of  an `NSError` object.
     enum Code: Int {
         /// The code will return if a request was cancelled.
         case cancelled = -999
